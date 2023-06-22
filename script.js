91bc4b2ffc8eb1f3ac7160374098f2265f24abfa
aman yadav <amanyadav592002@gmail.com>
2023-06-23 01:20:11 +0530

    third sem page complete

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ console.log("javascript connected");
 //         first.style.display = "none";
 //     }
 // }
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
